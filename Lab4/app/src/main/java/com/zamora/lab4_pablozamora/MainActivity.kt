d592d689c49702b90171a231ffcd41afc44f78c9
pabloozamora <pabloozamora02@gmail.com>
2022-08-11 22:08:44 -0600

    Lab 4 - Agregado Toast Message

--- Lab4/app/src/main/java/com/zamora/lab4_pablozamora/MainActivity.kt
+++ Lab4/app/src/main/java/com/zamora/lab4_pablozamora/MainActivity.kt
@@ -2,10 +2,25 @@ package com.zamora.lab4_pablozamora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnStart: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStart = findViewById(R.id.start_button)
+
+        initListeners()
+    }
+
+    private fun initListeners(){
+        btnStart.setOnClickListener{
+            Toast.makeText(this, "Pablo Andrés Zamora Vásquez", Toast.LENGTH_LONG).show()
+        }
     }
 }
