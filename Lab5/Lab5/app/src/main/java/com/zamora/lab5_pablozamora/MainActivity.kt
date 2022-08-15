4141ad5209a542d3e0d9067651ec04aacfed95d1
pabloozamora <pabloozamora02@gmail.com>
2022-08-15 02:09:34 -0600

    Inicio Lab 5

---
+++ Lab5/Lab5/app/src/main/java/com/zamora/lab5_pablozamora/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.zamora.lab5_pablozamora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnStart: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnStart = findViewById(R.id.start_button)
+
+        initListeners()
+    }
+
+    private fun initListeners(){
+        btnStart.setOnClickListener{
+            Toast.makeText(this, "Pablo Andrés Zamora Vásquez", Toast.LENGTH_LONG).show()
+        }
+    }
+}
